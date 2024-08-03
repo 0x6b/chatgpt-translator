02f39886dfec3defb5db5affbea3129127e2e21f
kaoru <k@warpnine.io>
2024-08-03 18:07:03 +0900

    feat: translate into two-column rich text format for documentation

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,9 @@
 //!
 //! An OpenAI-powered Markdown document translator. Translate your clipboard text into from/to any
 //! language.
-pub use translator::{Translator, TranslatorConfiguration};
+pub use document::Document;
+pub use translator::{ReadyForTranslation, Translator, TranslatorConfiguration};
 
+mod document;
 mod model;
 mod translator;
