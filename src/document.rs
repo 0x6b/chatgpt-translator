3efc7d12411dbd758c18b61f518287a869c03922
kaoru <k@warpnine.io>
2024-08-16 11:02:25 +0900

    docs: more code comments

--- src/document.rs
+++ src/document.rs
@@ -6,13 +6,18 @@ use markdown_split::split;
 
 use crate::{ReadyForTranslation, Translator};
 
+/// Represents a document to translate. It just holds the vector of strings. Each string is a
+/// section of the original text.
 pub struct Document {
+    /// Fragments of the text.
     pub fragments: Vec<String>,
 }
 
 impl TryFrom<String> for Document {
     type Error = anyhow::Error;
 
+    /// Try to create a new document from the given text. Text is split into sections based on
+    /// Markdown headings (h1-h6).
     fn try_from(text: String) -> Result<Self> {
         Ok(Self {
             fragments: split(&text, None)?
@@ -24,6 +29,15 @@ impl TryFrom<String> for Document {
 }
 
 impl Document {
+    /// Translate the document using the provided translator.
+    ///
+    /// # Arguments
+    ///
+    /// - `translator` - The translator to use.
+    ///
+    /// # Returns
+    ///
+    /// The [`Vec`] of translated text.
     pub async fn translate(
         &self,
         translator: &Translator<ReadyForTranslation>,
